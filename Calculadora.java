5dc95d30805d6eb7ee08d8c33e11da669d791289
Tiago86 <tiagoborges899@gmail.com>
2015-07-06 13:03:09 -0300

    Interface

---
+++ Calculadora.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tiago
+ */
+public class Calculadora {
+private int n1 , n2;
+
+    /**
+     * @return the n1
+     */
+    public double getN1() {
+        return n1;
+    }
+
+    /**
+     * @param n1 the n1 to set
+     */
+    public void setN1(int n1) {
+        this.n1 = n1;
+    }
+
+    /**
+     * @return the n2
+     */
+    public int getN2() {
+        return n2;
+    }
+
+    /**
+     * @param n2 the n2 to set
+     */
+    public void setN2(int n2) {
+        this.n2 = n2;
+    }
+
+
+}
